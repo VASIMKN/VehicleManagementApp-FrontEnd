ef3402ae1b676eb6bb1dd33cea5d3a5f7ab41a67
Apex-Mafia <vas007im@gmail.com>
2021-06-01 21:33:40 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookingComponent } from './booking/booking.component';
+import { DriverComponent } from './components/driver/driver.component';
+import { UserComponent } from './components/user/user.component';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingComponent,
+    DriverComponent,
+    UserComponent,
+    VehicleComponent,
+    CustomerComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
